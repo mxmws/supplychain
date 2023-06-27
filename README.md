6bb86aa7c4130fd908c5351fae5e298c5716d0b8
mxmws <maximilian.mews@gmail.com>
2023-06-27 21:41:16 +0200

    changed structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Instructions
+## Getting started
 
 ### Setup Metamask with Goerli Testnetwork
 
@@ -11,20 +11,6 @@
    - If the RPC URL doesn't work, search for a new one [here](https://chainlist.org/chain/5).
 3. Mine at least 0.02 Goerli ETH from [Goerli Faucet](https://goerli-faucet.pk910.de/).
 
-### Deploy Contract to Goerli Testnet
-
-To deploy a contract on the Goerli Testnet, follow the steps outlined in this [guide](https://vulehuan.com/en/blog/2023/05/deploy-your-blockchain-contract-on-remix-with-metamask-and-goerli-64680f9664ac40ae37cdf47a.html#gsc.tab=0). You may need between 0.1 and 0.5 GöETH for this process.
-
-### Estimate Cost of Deploying a Smart Contract
-
-Use the following resources to estimate the cost of deploying a smart contract:
-- [Smart Contract Deployment Cost Calculator](https://www.rareskills.io/smart-contract-deployment-cost-calculator)
-- [JavaInUse: Smart Contract Deployment Cost](https://www.javainuse.com/bytesize)
-
-### Host a Node
-
-*(Information on hosting a node is not provided.)*
-
 ### Setup Frontend
 
 To set up the frontend:
@@ -35,34 +21,11 @@ To set up the frontend:
 4. Add the key to the appropriate file.
 5. Run the script using `node ./frontend/script.js` to test if everything is working properly.
 
+### Deploy Contract to Goerli Testnet
 
+To deploy a contract on the Goerli Testnet, follow the steps outlined in this [guide](https://vulehuan.com/en/blog/2023/05/deploy-your-blockchain-contract-on-remix-with-metamask-and-goerli-64680f9664ac40ae37cdf47a.html#gsc.tab=0). You may need between 0.1 and 0.5 GöETH for this process.
 
+### Host a Node
 
-REMIX DEFAULT WORKSPACE
+*(Information on hosting a node is not provided.)*
 
-Remix default workspace is present when:
-i. Remix loads for the very first time 
-ii. A new workspace is created with 'Default' template
-iii. There are no files existing in the File Explorer
-
-This workspace contains 3 directories:
-
-1. 'contracts': Holds three contracts with increasing levels of complexity.
-2. 'scripts': Contains four typescript files to deploy a contract. It is explained below.
-3. 'tests': Contains one Solidity test file for 'Ballot' contract & one JS test file for 'Storage' contract.
-
-SCRIPTS
-
-The 'scripts' folder has four typescript files which help to deploy the 'Storage' contract using 'web3.js' and 'ethers.js' libraries.
-
-For the deployment of any other contract, just update the contract's name from 'Storage' to the desired contract and provide constructor arguments accordingly 
-in the file `deploy_with_ethers.ts` or  `deploy_with_web3.ts`
-
-In the 'tests' folder there is a script containing Mocha-Chai unit tests for 'Storage' contract.
-
-To run a script, right click on file name in the file explorer and click 'Run'. Remember, Solidity file must already be compiled.
-Output from script will appear in remix terminal.
-
-Please note, require/import is supported in a limited manner for Remix supported modules.
-For now, modules supported by Remix are ethers, web3, swarmgw, chai, multihashes, remix and hardhat only for hardhat.ethers object/plugin.
-For unsupported modules, an error like this will be thrown: '<module_name> module require is not supported by Remix IDE' will be shown.
---
+++ remix/contracts/supplychain.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "./Product.sol";
+import "./Label.sol";
+
+contract Supplychain {
+    struct label {
+        bool isValue;
+        string name;
+        uint productId;
+    }
+
+    mapping (address => Product) products;
+    mapping (address => label) labels;
+
+    event ProductAdded(
+        address indexed _user,
+        address _id,
+        string _name
+    );
+
+    event LabelAdded(
+        address indexed _user,
+        address _id,
+        string _name
+    );
+
+    event LinkAdded(
+        address indexed _user,
+        address _predecessorProductId,
+        address _successorProductId
+    );
+
+    event LinkRemoved(
+        address indexed _user,
+        address _predecessorProductId,
+        address _successorProductId
+    );
+
+    // this function shouldn't cost gas (not sure why it says infinite)
+    function getProduct(address _id) public view returns(Product){
+        // require(labels[_id].GetOwner() != address(0), "Product doesn't exist");
+        Product myProduct = products[_id];
+        return myProduct;
+    }
+    
+    function addLabel(address _id, string memory _name, uint _productId) public {
+        require(!labels[_id].isValue, "Label with this ID already exists");
+        labels[_id] = label(true, _name, _productId);
+        // ToDo: Handshake
+        emit LabelAdded(msg.sender, _id, _name);
+    }
+
+    function addProduct(address _address, string memory _name, address[] memory _successors, address[] memory _predecessors, uint _carbonFootprint) public {
+        require(products[_address].owner() != address(0), "Product with this ID already exists");
+
+        // this is probably not the best way to do it but can't use constructor because struct contains mappings
+        Product prod = new Product(_name, _carbonFootprint);
+        prod.Set_SuccessorIds(_successors);
+        prod.Set_PredecessorIds(_predecessors);
+        prod.Set_CarbonFootPrint(_carbonFootprint);
+
+        // ToDo: Handshake
+        emit ProductAdded(msg.sender, _address, _name); // event will be on the blockchain forever
+    }
+
+    function addLink(address _predecessorProductId, address _successorProductId) public  {
+        // ToDo: Handshake / checking ownership
+        //1. check product in othter contract if self was added as candidate   
+            //no: enter other address into respective candidate mapping
+            //return
+        //2. yes: delete self in other candidate mapping
+        //3. update own successor/predecessor mapping
+        //4. update other successor/predecessor mapping
+            //--> how to handle ownership and stop attackers from deleting random products
+        
+        // ToDo: make sure link doesn't exist yet
+        
+
+        // check if both products exist
+        Product predecessor = products[_predecessorProductId]; 
+        Product successor = products[_successorProductId];
+        require(predecessor.isValue() && successor.isValue(), "One or both products don't exist");
+
+        if(predecessor.Do_Handshake_To_Successor(_successorProductId)){
+            //handshake successfull, predecessor_candidate of successor was removed (set to false)
+        }
+        else{
+            successor.Do_Handshake_To_Predecessor(_predecessorProductId);
+        }
+
+        emit LinkAdded(msg.sender, _predecessorProductId, _successorProductId);
+    }
+
+    function removeLink(address _predecessorProductId, address _successorProductId) public {
+        // Todo: Problem: searching an array can be very expensive
+        // check if both products exist
+        require(products[_predecessorProductId].isValue() && products[_successorProductId].isValue(), "One or both products don't exist");
+
+        // get products
+        Product predecessor = products[_predecessorProductId];
+        Product successor = products[_successorProductId];
+
+        // remove links from products
+        uint predecessorIndex;
+        uint successorIndex;
+
+        // find index of _successorProductId in predecessor's successors
+        for (uint i = 0; i < predecessor.Get_Successor_Count(); i++) {
+            if (predecessor.successorIds(i) == _successorProductId) {
+                predecessorIndex = i;
+                break;
+            }
+        }
+
+        // find index of _predecessorProductId in successor's predecessors
+        for (uint i = 0; i < successor.Get_Predecessor_Count(); i++) {
+            if (successor.predecessorIds(i) == _predecessorProductId) {
+                successorIndex = i;
+                break;
+            }
+        }
+
+        // remove the links
+        // delete products[_predecessorProductId].successorIds[predecessorIndex];
+        // delete products[_successorProductId].predecessorIds[successorIndex];
+
+        emit LinkRemoved(msg.sender, _predecessorProductId, _successorProductId);
+    }
+
+}
