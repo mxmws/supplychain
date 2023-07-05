2f53215cb5d85ccd02c71f9e1885edd5a12b5b3a
mxmws <59613435+mxmws@users.noreply.github.com>
2023-07-05 18:29:30 +0200

    Update supplychain.sol

--- contracts/supplychain.sol
+++ contracts/supplychain.sol
@@ -3,37 +3,11 @@ pragma solidity 0.8.18;
 
 import "./Product.sol";
 import "./Label.sol";
-import "./Ownable.sol";
-import "./Label.sol";
 
 contract Supplychain {
-     
+
     mapping (address => Product) products;
     mapping (address => Label) labels;
-
-
-    event ContractAdded(
-        string ContractType,
-        address indexed _user,
-        address _id,
-        string _name
-    );
-
-    event LinkAdded(
-        address indexed _user,
-        address _predecessorProductId,
-        address _successorProductId
-    );
-
-    event LinkRemoved(
-        address indexed _user,
-        address _predecessorProductId,
-        address _successorProductId
-    );
-
-    event ProductCreated(
-        address indexed productAddress
-    );
 
 
     function getProduct(address _id) public view returns (
@@ -55,40 +29,41 @@ contract Supplychain {
         );
     }
     
-    function addLabel(address _id, string memory _name, uint _productId) public {
-        require(!labels[_id].IsValue(), "Label with this ID already exists");
-        labels[_id] = new Label(_name, _productId);
-        // ToDo: Handshake
-        emit ContractAdded("Label", msg.sender, _id, _name);
+    function addLabel(
+        string memory _name,
+        address[] memory _ProductIDs,
+        string memory _ipfsAddress
+    ) public returns (address) {
+        Label label = new Label(_name, msg.sender);
+
+        label.Set_ProductIds(_ProductIDs);
+        label.Set_IPFS(_ipfsAddress);
+
+        labels[address(label)] = label;
+        return address(label);
     }
 
     function addProduct(
         string memory _name,
         uint _carbonFootprint,
-        address[] memory _labelIDs,
-        address[] memory _successors,
-        address[] memory _predecessors,
+        //address[] memory _labelIDs,
+        //address[] memory _successors,
+        //address[] memory _predecessors,
         string memory _ipfsAddress
     ) public returns (address) {
         Product prod = new Product(_name, _carbonFootprint, msg.sender);
 
-        prod.Set_SuccessorCandadiateIds(_successors);
-        prod.Set_PredecessorCandidateIds(_predecessors);
+        //prod.Set_SuccessorCandadiateIds(_successors);
+        //prod.Set_PredecessorCandidateIds(_predecessors);
+        //prod.Set_LabelCandidateIds(_labelIDs);
         prod.Set_CarbonFootPrint(_carbonFootprint);
-        prod.Set_LabelIds(_labelIDs);
         prod.Set_IPFS(_ipfsAddress);
 
         products[address(prod)] = prod;
 
-        emit ProductCreated(address(prod));
-
         return address(prod);
     }
 
-    event CheckpointReached(
-        string name,
-        uint16 line
-    );
 
     function addLink(address _predecessorProduct, address _successorProduct) public returns(bool handshake_complete)  {
 
@@ -97,7 +72,7 @@ contract Supplychain {
 
         if(msg.sender == predecessor.Get_Owner())
         {
-            bool success = predecessor.Do_Handshake_To_Successor(_successorProduct);            
+            bool success = predecessor.Do_Handshake(_successorProduct, relation.SUCCESSOR);            
         
             if(predecessor.Get_Owner() != successor.Get_Owner())
             return success;
@@ -105,7 +80,7 @@ contract Supplychain {
 
         if(msg.sender == successor.Get_Owner())
         {
-            bool success =  successor.Do_Handshake_To_Predecessor(_predecessorProduct);
+            bool success =  successor.Do_Handshake(_predecessorProduct, relation.PREDECESSOR);
             
             return success;
         }
@@ -114,22 +89,33 @@ contract Supplychain {
             require(false, "Message sender is not the owner of either product");
         }
     }
-
+/*
     function removeLink(address _predecessorProductId, address _successorProductId) public {
         // check if both products exist
         require(products[_predecessorProductId].Get_IsValue() && products[_successorProductId].Get_IsValue(), "One or both products don't exist");
         // get products
-        Product predecessor = products[_predecessorProductId];
-        Product successor = products[_successorProductId];
+        Handshaker predecessor = products[_predecessorProductId];
+        Handshaker successor = products[_successorProductId];
 
         require(msg.sender == predecessor.Get_Owner() || msg.sender == successor.Get_Owner(), "Links can only be removed by either of the owners");
         // remove links from products
-        predecessor.Remove_Successor(_successorProductId);
-        successor.Remove_Predecessor(_predecessorProductId);
-
-        emit LinkRemoved(msg.sender, _predecessorProductId, _successorProductId);
+        address[] storage arr = predecessor.Ids[relation.SUCCESSOR];
+        uint len = arr.length;
+        for (uint i = 0; i < len; i++) {
+            if (successor.Ids[relation.SUCCESSOR][i] == _predecessorProductId) {
+                delete successor.Ids[relation.SUCCESSOR][i];
+                break;
+            }
+        }
+        len = successor.Ids[relation.SUCCESSOR].length;
+        for (uint i = 0; i < len; i++) {
+            if (predecessor.Ids[relation.PREDECESSOR][i] == _successorProductId) {
+                delete predecessor.Ids[relation.PREDECESSOR][i];
+                break;
+            }
+        }
     }
-
+*/
     //CONTRACT BECOMES TOO LARGE IF ANOTHER METHOD IS ADDDED 
 
     // function Change_Name(string calldata name, address productId) public {
