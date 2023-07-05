577af45e9404f5077b73fa0b2245928eb863e4dd
mxmws <59613435+mxmws@users.noreply.github.com>
2023-07-05 18:29:12 +0200

    Update Product.sol

--- contracts/Product.sol
+++ contracts/Product.sol
@@ -1,58 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "./Ownable.sol";
+import "./Handshaker.sol";
 
-contract Product is Ownable{
-
-    struct Instance{
-        address Owner;
-        bool IsValue;
-        string Name;
-        mapping(address => bool) SuccessorIdIsHandshakeCandidate;
-        mapping(address => bool) PredecessorIdIsHandshakeCandidate;
-
-        mapping(uint => uint) SuccessorIdToArrayIndex;
-        mapping(uint => uint) PredecessorIdToArrayIndex;
-
-        address[] SuccessorIds; 
-        address[] PredecessorIds;
-
-        address[] LabelIds;
-
-        string Ipfs_Address;
-
-        uint CarbonFootprint;
-
-        string Swarm_storage_address;
-    }
+contract Product is Handshaker{
     
-    mapping(uint256 => Instance) public products;
+
+    bool IsValue;
+    string Name;
+
+    string Ipfs_Address;
+
+    uint CarbonFootprint;
+
+    string Swarm_storage_address;
+    
+    mapping(uint256 => Product) public products;
     mapping(address => uint256[]) public userProducts;
 
-    Instance instance;
-
-    event ProductCreated(
-        address indexed productAddress
-    );
-
-    event HandShakeDone(
-        string Name,
-        bool Successful
-    );
 
     constructor(string memory _name, uint _carbonFootprint, address _owner) {
-        instance.Owner = _owner;
-        instance.Name = _name;
-        instance.CarbonFootprint = _carbonFootprint;
-        instance.IsValue= true;
-
-        emit ProductCreated(address(this));
+        Owner = _owner;
+        Name = _name;
+        CarbonFootprint = _carbonFootprint;
+        IsValue= true;
 
     }
     //region setter functions
     function Set_Name(string calldata new_name) public onlyOwner{
-        instance.Name = new_name;
+        Name = new_name;
     }
     //SUPPLYCHAIN CONTRACT BECOMES TOO BIG IF THIS WAY OF OWNERSHIP TEST GETS IMPLEMENTED:
     // function Set_Name(string calldata new_name, address _owner) public onlyOwner{
@@ -60,160 +36,56 @@ contract Product is Ownable{
     //     instance.Name = new_name;
     // }
 
-    function Set_SuccessorIds(address[] calldata new_successorIds) public onlyOwner{
-        instance.SuccessorIds = new_successorIds;
-    }
-
-    function Set_SuccessorCandadiateIds(address[] calldata new_successorIds) public onlyOwner{
-       for(uint i = 0; i < new_successorIds.length; i++){
-            instance.PredecessorIdIsHandshakeCandidate[new_successorIds[i]] = true;
-        }
-    }
-
-    function Set_PredecessorCandidateIds(address[] calldata new_predecessorIds) public onlyOwner{
-        for(uint i = 0; i < new_predecessorIds.length; i++){
-            instance.PredecessorIdIsHandshakeCandidate[new_predecessorIds[i]] = true;
-        }
-    }
-
-    function Set_PredecessorIds(address[] calldata new_predecessorIds) public onlyOwner{
-        instance.SuccessorIds = new_predecessorIds;
-    }
-
-    function Set_LabelIds(address[] calldata new_LabelIds) public onlyOwner{
-        instance.SuccessorIds = new_LabelIds;
-    }
+    
 
     function Set_CarbonFootPrint(uint new_CarbonFootprint) public onlyOwner{
-        instance.CarbonFootprint = new_CarbonFootprint;
+        CarbonFootprint = new_CarbonFootprint;
     }
 
     function Set_IsValue(bool value) public onlyOwner{
-        instance.IsValue = value;
+        IsValue = value;
     }
 
     function Set_IPFS(string calldata _ipfsAddress) public onlyOwner{
-        instance.Ipfs_Address = _ipfsAddress;
+        Ipfs_Address = _ipfsAddress;
     }
     //endregion setter functions
 
     //region getter functios
     function Get_Successor_Count() external view returns(uint256){
-        return instance.SuccessorIds.length;
+        return Ids[relation.SUCCESSOR].length;
     }
 
     function Get_Predecessor_Count() external view returns(uint256){
-        return instance.PredecessorIds.length;
+        return Ids[relation.PREDECESSOR].length;
     }
 
     function Get_Predecessors() external view returns(address[] memory){
-        return instance.PredecessorIds;
+        return Ids[relation.PREDECESSOR];
     }
     function Get_Successors() external view returns(address[] memory){
-        return instance.SuccessorIds;
+        return Ids[relation.SUCCESSOR];
     }
     function Get_LabelIDs() external view returns(address[] memory){
-        return instance.LabelIds;
+        return Ids[relation.LABEL];
     }
 
     function Get_IpfsAddress() external view returns(string memory){
-        return instance.Ipfs_Address;
+        return Ipfs_Address;
     }
 
     function Get_Name() external view returns (string memory){
-        return instance.Name;
+        return Name;
     }
 
     function Get_IsValue() external view returns(bool){
-        return instance.IsValue;
+        return IsValue;
     }
 
     function Get_CarbonFootprint() external  view returns(uint){
-        return instance.CarbonFootprint;
+        return CarbonFootprint;
     }
 
-    function Get_Owner() external view returns(address owner){
-        return instance.Owner;
-    }
     // endregion getter functions
-
-    //region hand shake
-    function Has_SuccessorCandidate(address successorAddress) public view returns(bool){
-        return instance.SuccessorIdIsHandshakeCandidate[successorAddress];
-    }
-
-    function Has_PredecessorCandidate(address predecessorAddress) public view returns(bool){
-        return instance.SuccessorIdIsHandshakeCandidate[predecessorAddress];
-    }
-    
-    function Accept_SuccessorCandidate() public {
-        require(instance.SuccessorIdIsHandshakeCandidate[msg.sender] == true, "Requestor is not a candidate for successorship.");
-        instance.SuccessorIdIsHandshakeCandidate[msg.sender] = false;
-        instance.SuccessorIds.push(msg.sender);
-
-    }
-
-    function Accept_PredecessorCandidate() public {
-        require(instance.PredecessorIdIsHandshakeCandidate[msg.sender] == true, "Requestor is not a candidate for predecessorship.");
-        instance.PredecessorIdIsHandshakeCandidate[msg.sender] = false;
-        instance.PredecessorIds.push(msg.sender);
-    }
-
-    function Do_Handshake_To_Successor(address successor_address) public returns(bool){
-        Product successor = Product(successor_address);
-
-        if(successor.Has_PredecessorCandidate(address(this))){
-            instance.SuccessorIds.push(successor_address);
-            successor.Accept_PredecessorCandidate();
-            
-            emit HandShakeDone("Successor", true);
-            return true;
-        }
-        else{
-            instance.SuccessorIdIsHandshakeCandidate[successor_address] = true;            
-            
-            emit HandShakeDone("Successor", false);
-            return false;
-        }
-    }
-    
-    function Do_Handshake_To_Predecessor(address predecessor_address) public returns(bool){
-        Product predecessor = Product(predecessor_address);
-
-        if(predecessor.Has_PredecessorCandidate(address(this))){
-            instance.PredecessorIds.push(predecessor_address);
-            predecessor.Accept_SuccessorCandidate();
-            
-            emit HandShakeDone("Predecessor", true);
-            return true;
-        }
-        else{
-            instance.PredecessorIdIsHandshakeCandidate[predecessor_address];
-            
-            emit HandShakeDone("Predecessor", false);
-            return false;
-        }
-    }
-    //endregion handshake
-
-    //region delete predecessors / successors
-    function Remove_Predecessor(address _predecessor) public onlyOwner{
-        for (uint i = 0; i < instance.PredecessorIds.length; i++) {
-            if (instance.PredecessorIds[i] == _predecessor) {
-                delete instance.PredecessorIds[i];
-                break;
-            }
-        }
-    }
-
-    function Remove_Successor(address _successor) public onlyOwner{
-        for (uint i = 0; i < instance.SuccessorIds.length; i++) {
-            if (instance.SuccessorIds[i] == _successor) {
-                delete instance.SuccessorIds[i];
-                break;
-            }
-        }
-    }
-    //endregion delete predecessors / successors
 
 }
