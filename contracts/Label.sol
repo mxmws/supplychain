e2bdb4dbf401af64ad47dc97e70983ab1c4bfd15
mxmws <59613435+mxmws@users.noreply.github.com>
2023-07-07 18:43:18 +0200

    Update Label.sol

--- contracts/Label.sol
+++ contracts/Label.sol
@@ -4,39 +4,20 @@ pragma solidity 0.8.18;
 import "./Handshaker.sol";
 
 contract Label is Handshaker{
-    struct Instance {
-        address Owner;
-        bool IsValue;
-        string Name;
-        string Ipfs_Address;
+    bool IsValue;
+    string Name;
+    string Swarm_storage_address;
 
-        mapping(address => bool) ProductIdIsHandshakeCandidate;
-        address[] ProductIds;
-    }
+    address[] ProductIds;
 
-    Instance public instance;
-    
 
     constructor(string memory _name, address _owner) {
-        instance.Owner = _owner;
-        instance.Name = _name;
-        instance.IsValue= true;
-
+        Owner = _owner;
+        Name = _name;
+        IsValue= true;
     }
 
     function Set_IPFS(string calldata _ipfsAddress) public onlyOwner{
-        instance.Ipfs_Address = _ipfsAddress;
-    }
-
-    function Set_ProductIds(address[] calldata new_ProductIds) public onlyOwner{
-        instance.ProductIds = new_ProductIds;
-    }
-
-    function IsValue() public view returns(bool){
-        return instance.IsValue;
-    }
-
-    function Name() public view returns(string memory){
-        return instance.Name;
+        Swarm_storage_address = _ipfsAddress;
     }
 }
